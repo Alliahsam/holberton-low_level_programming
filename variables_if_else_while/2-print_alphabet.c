c07a4a00b3b007eee21e9820785aaa0e69e77ce2
Alliahsam <alliah2@hotmail.fr>
2023-02-23 07:42:05 -0800

    tache 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main Prints alphabet
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char alphab;
+	for (alphab='a'; alphab<='z' ; alphab++){
+		putchar(alphab);
+	}
+	return(0);
+}
