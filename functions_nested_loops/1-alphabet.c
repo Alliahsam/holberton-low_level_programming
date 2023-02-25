b0b0096cbce04ddb2c53cb59e622341524a4653c
Alliahsam <alliah2@hotmail.fr>
2023-02-25 14:15:32 -0800

    tache 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
   * print_alphabet - Make the alphabet
@@ -11,7 +11,7 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 
 	_putchar('\n');
