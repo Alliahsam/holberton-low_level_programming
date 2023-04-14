19bef97afec4c1d13537ee6a6aaf860f57a341c1
Alliahsam <alliah2@hotmail.fr>
2023-04-14 14:57:11 -0700

    modif hash_tables.h

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,18 +1,27 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-/**
- * hash_table_create - Create a new hash table.
- *
- * @size: Size of the new hash table.
- *
- * Return: Pointer to the new hash table or NULL if fails.
- */
-
-int main(void)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	unsigned long int i = 0;
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
+	if (!new_table)
+		return (NULL);
+
+	new_table->size = size;
+
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+		new_table->array[i] = NULL;
+
+	return (new_table);
 }
